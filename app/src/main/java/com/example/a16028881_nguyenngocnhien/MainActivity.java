e50fb45c3edd110fdf23714701d89a52f460bbbe
Nhien <63007332+tinieee@users.noreply.github.com>
2021-12-20 18:07:58 +0700

    prj
    prj

---
+++ app/src/main/java/com/example/a16028881_nguyenngocnhien/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a16028881_nguyenngocnhien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
